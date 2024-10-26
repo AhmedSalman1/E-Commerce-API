f37584007eb0186eb373ad5d8ed6f53678bbaf9e
AhmedSalman <medosalman375@gmail.com>
2024-10-26 18:13:06 +0300

    refactor: clean up and optimize APIFeatures

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,61 +2,22 @@ import slugify from 'slugify';
 import catchAsyncError from 'express-async-handler';
 import { Product } from '../models/product.model.js';
 import { AppError } from '../utils/appError.js';
+import { APIFeatures } from '../utils/apiFeatures.js';
 
 export const getAllProducts = catchAsyncError(async (req, res) => {
-  //! Filtering
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'limit', 'sort', 'fields', 'keyword'];
-  excludedFields.forEach((el) => delete queryObj[el]);
+  const features = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .search()
+    .sort()
+    .limitFields()
+    .paginate();
 
-  //* Advanced filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  //! Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 50;
-  const skip = (page - 1) * limit;
-
-  //! Search
-  let filterConditions = JSON.parse(queryStr);
-
-  if (req.query.keyword) {
-    filterConditions.$or = [
-      { title: { $regex: req.query.keyword, $options: 'i' } },
-      { description: { $regex: req.query.keyword, $options: 'i' } },
-    ];
-  }
-
-  //* Build query
-  let query = Product.find(filterConditions)
-    .skip(skip)
-    .limit(limit)
-    .populate('category subcategories', 'name -_id');
-
-  //! Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  //! Field limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-  } else {
-    query = query.select('-__v');
-  }
-
-  //* Execute query
-  const products = await query;
+  // Execute query
+  const products = await features.query;
 
   res.status(200).json({
     status: 'success',
     results: products.length,
-    page,
     data: {
       products,
     },
