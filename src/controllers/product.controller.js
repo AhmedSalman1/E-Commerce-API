d58b5f1917b881c0bd67b7ef10603c347ea6b0c5
AhmedSalman <medosalman375@gmail.com>
2024-10-26 15:11:11 +0300

    feat: Add search, filtering, sorting, etc. for product queries and dummy data

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,14 +4,54 @@ import { Product } from '../models/product.model.js';
 import { AppError } from '../utils/appError.js';
 
 export const getAllProducts = catchAsyncError(async (req, res) => {
+  //! Filtering
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'limit', 'sort', 'fields', 'keyword'];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  //* Advanced filtering
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+  //! Pagination
   const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 9;
+  const limit = req.query.limit * 1 || 50;
   const skip = (page - 1) * limit;
 
-  const products = await Product.find()
+  //! Search
+  let filterConditions = JSON.parse(queryStr);
+
+  if (req.query.keyword) {
+    filterConditions.$or = [
+      { title: { $regex: req.query.keyword, $options: 'i' } },
+      { description: { $regex: req.query.keyword, $options: 'i' } },
+    ];
+  }
+
+  //* Build query
+  let query = Product.find(filterConditions)
     .skip(skip)
     .limit(limit)
     .populate('category subcategories', 'name -_id');
+
+  //! Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  //! Field limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+
+  //* Execute query
+  const products = await query;
 
   res.status(200).json({
     status: 'success',
