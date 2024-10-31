d3c8452020826b1ed8f4cfcb9e6b9119d130e40b
AhmedSalman <medosalman375@gmail.com>
2024-10-31 11:48:38 +0300

    feat: implement brand image upload functionality

--- src/controllers/brand.controller.js
+++ src/controllers/brand.controller.js
@@ -1,5 +1,30 @@
+import sharp from 'sharp';
+import { v4 as uuidv4 } from 'uuid';
+import catchAsyncError from 'express-async-handler';
+
 import * as factory from './handlerFactory.js';
 import { Brand } from '../models/brand.model.js';
+import { uploadSingleImage } from '../middlewares/uploadImageMiddleware.js';
+
+export const uploadBrandImage = uploadSingleImage('image');
+
+export const resizeBrandImage = catchAsyncError(async (req, res, next) => {
+  // console.log(req.file);
+  if (!req.file) return next();
+
+  const filename = `brand-${uuidv4()}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(400, 400)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/brands/${filename}`);
+
+  // Save image name in DB
+  req.body.image = filename;
+
+  next();
+});
 
 export const getAllBrands = factory.getAll(Brand);
 export const getBrand = factory.getOne(Brand);
