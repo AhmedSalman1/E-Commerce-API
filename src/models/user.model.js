6c45fda7dcb8df62da28ddf66bec3c7d0e3ceba5
AhmedSalman <medosalman375@gmail.com>
2024-11-06 14:00:32 +0200

    feat(auth): add password confirmation and hashing for user creation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -42,5 +43,12 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
 export const User = mongoose.model('User', userSchema);
--- src/utils/validators/userValidator.js
+++ src/utils/validators/userValidator.js
@@ -35,7 +35,17 @@ export const createUserValidator = [
     .notEmpty()
     .withMessage('Password is required!')
     .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters long!'),
+    .withMessage('Password must be at least 8 characters long!')
+    .custom((password, { req }) => {
+      if (password !== req.body.passwordConfirm) {
+        throw new Error('Passwords do not match!');
+      }
+      return true;
+    }),
+
+  check('passwordConfirm')
+    .notEmpty()
+    .withMessage('Password confirmation is required!'),
 
   check('phone')
     .optional()
