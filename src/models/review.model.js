80f2058acbe4765b1cba5dd39a4614f4601340d7
AhmedSalman <medosalman375@gmail.com>
2024-11-14 15:23:52 +0200

    feat: add Review schema

---
+++ src/models/review.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Rating is required!'],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a User!'],
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: [true, 'Review must belong to a Product!'],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model('Review', reviewSchema);
