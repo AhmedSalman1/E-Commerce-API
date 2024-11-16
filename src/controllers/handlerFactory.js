955c4a04b558c7ded0fdc3148c5eb4214606e37f
AhmedSalman <medosalman375@gmail.com>
2024-11-16 20:30:39 +0200

    feat(reviews): calculate ratings quantity and average for create, update, and delete operations

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -74,6 +74,8 @@ export const updateOne = (Model) =>
       );
     }
 
+    // Trigger 'save' event when update doc
+    doc.save();
     res.status(200).json({
       status: 'success',
       data: doc,
@@ -89,6 +91,12 @@ export const deleteOne = (Model) =>
       return next(
         new AppError(`${Model.modelName} with ID ${id} not found`, 404)
       );
+    }
+
+    if (Model.modelName === 'Review') {
+      const productId = doc.product;
+
+      await Model.calcAvgRatingsAndQuantity(productId);
     }
 
     res.status(204).json({
--- src/models/review.model.js
+++ src/models/review.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Product } from './product.model.js';
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -28,6 +29,38 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: 'user', select: 'name' });
   next();
+});
+
+/*                  Static Methods                  */
+reviewSchema.statics.calcAvgRatingsAndQuantity = async function (productId) {
+  const stats = await this.aggregate([
+    {
+      $match: { product: productId },
+    },
+    {
+      $group: {
+        _id: 'product',
+        ratingsQuantity: { $sum: 1 },
+        avgRatings: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsQuantity: stats[0].ratingsQuantity,
+      ratingsAverage: stats[0].avgRatings,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 0,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAvgRatingsAndQuantity(this.product);
 });
 
 export const Review = mongoose.model('Review', reviewSchema);
