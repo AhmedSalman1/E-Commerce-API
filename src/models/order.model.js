1488e525f453937928e386652e6775ca31b1e850
AhmedSalman <medosalman375@gmail.com>
2024-11-18 06:38:57 +0200

    feat: add Order schema

---
+++ src/models/order.model.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Order must belong to a User!'],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: Number,
+        color: String,
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingAddress: {
+      details: String,
+      phone: String,
+      city: String,
+      postalCode: String,
+    },
+    totalPrice: {
+      type: Number,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['cash', 'card'],
+      default: 'cash',
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
