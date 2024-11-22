7f06208df6ac31d1ce147fba0db6c95d2aead66a
AhmedSalman <medosalman375@gmail.com>
2024-11-22 14:02:09 +0200

    feat: add custom error handling for DB errors(duplicate, cast, validation)

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -6,6 +6,25 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError('Your token has expired! Please log in again.', 401);
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, req, res) => {
   if (req.originalUrl.startsWith('/api')) {
     return res.status(err.statusCode).json({
@@ -46,6 +65,9 @@ export const globalErrorHandler = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     if (err.name === 'JsonWebTokenError') err = handleJWTError();
     if (err.name === 'TokenExpiredError') err = handleJWTExpiredError();
+    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === 'CastError') err = handleCastErrorDB(err);
+    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
     sendErrorProd(err, req, res);
   }
 };
